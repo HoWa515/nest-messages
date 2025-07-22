18891d38dce06ea6716b2ac3bfa121aa180efbd3
wh <sunfortea123@gmail.com>
2025-07-22 17:05:03 +0200

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'Hello';
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+    return 'created';
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+    return 'id id';
+  }
+}
