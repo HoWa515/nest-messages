e192a95c2369be132311c3b646b3539a3fe681b2
wh <sunfortea123@gmail.com>
2025-07-25 16:25:15 +0200

    m

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,39 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
 @Controller('messages')
 export class MessagesController {
+  // DO NOT do this in real app
+  messagesService: MessagesService;
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    return 'Hello';
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
-    return 'created';
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
-    return 'id ';
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('404 , Message not found');
+    }
+
+    return message;
   }
 }
